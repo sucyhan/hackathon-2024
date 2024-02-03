2967c545c7a7082e53a5ac3baf4f45d122680a67
Anneraymond08 <anneraymond08@gmail.com>
2024-02-03 12:48:00 -0500

    AR: create home page

---
+++ client/src/pages/Home-page.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/siteLogo.png"
+import "./Home-page.css"
+
+function Homepage() {
+    return (
+        <div>
+            <img src={logo} alt="logo"></img>
+            <p id="slogan">Sauver la planète, un repas à la fois</p>
+            <p>Bienvenue sur notre site de réduction des déchets alimentaires!</p>
+        </div>
+    )
+}
+
+
+export default Homepage;
