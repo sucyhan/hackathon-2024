1300f4ecd58bc4922e361942754bc1480b4f37de
Anneraymond08 <anneraymond08@gmail.com>
2024-02-04 09:11:42 -0500

    AR: make to use list work

--- client/src/components/ToUseList.jsx
+++ client/src/components/ToUseList.jsx
@@ -1,29 +1,56 @@
-// import { TableBody, TableCell, TableHead, TableRow } from '@mui/material';
-// import Table from '@mui/material/Table';
-// import "./ToUseList.css";
+import { Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import Table from '@mui/material/Table';
+import { useQuery } from "react-query";
+import { useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { getData } from '../services/inventory.service';
+import "./ToUseList.css";
 
-// function ToUseList() {
+function ToUseList() {
+    const { user } = useAuth0();
+    const [inventoryList, setInventoryList] = useState([]);
+    const { data, isFetched } = useQuery("inventory", async () => await getData(user.email), {
+        onSuccess: (data) => {
+            console.log(data);
+            return data;
+        }
+    });
 
-//     let data = [];
+    useEffect(() => {
+        if (isFetched && data) {
+            setInventoryList([...data]);
+        }
+    }, [data, isFetched]);
 
+    return (
+        <div className=''>
+            <p className='title'>À utiliser au plus vite...</p>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Aliment</TableCell>
+                                <TableCell>Type</TableCell>
+                                <TableCell>Date d'expiration</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        {!isFetched && inventoryList.length === 0 ? (<TableRow><TableCell colSpan={6}>Aucun aliment trouvé</TableCell></TableRow>) :
+                            (<TableBody>
+                                {inventoryList.map((item, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>{item.ingredient}</TableCell>
+                                        <TableCell>{item.foodType}</TableCell>
+                                        <TableCell>{item.expirationDate}</TableCell>
+                                    </TableRow>
+                                ))}
 
-//     return (
-//         <div className=''>
-//             <p className='title'>À utiliser au plus vite...</p>
-//             <Table stickyHeader>
-//                 <TableHead>
-//                     <TableRow>
-//                         <TableCell>Aliment</TableCell>
-//                         <TableCell>Date d'expiration</TableCell>
-//                         <TableCell>Nombre de jours restants</TableCell>
-//                     </TableRow>
-//                 </TableHead>
-//                 {data.length === 0 && (<TableRow><TableCell colSpan={6}>Aucun aliment trouvé</TableCell></TableRow>)}
-//                 {data.length !== 0 && (data.map())}
+                            </TableBody>)}
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </div>
+    )
+}
 
-//             </Table>
-//         </div>
-//     )
-// }
-
-// export default ToUseList;
+export default ToUseList;
